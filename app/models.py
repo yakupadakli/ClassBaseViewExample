375fad4bc347c76e688040becfba45924778521e
yakupadakli <yakup.adakli@hotmail.com>
2013-09-25 10:06:00 +0300

    model created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    class Meta:
+        ordering = ["-name"]
+
+    def __unicode__(self):
+        return self.name
+
+
+class Author(models.Model):
+    salutation = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    head_shot = models.ImageField(upload_to='author_head_shots')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
+
+    def __unicode__(self):
+        return  self.title
